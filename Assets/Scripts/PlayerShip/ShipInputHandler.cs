f6a7167664fa0cbebee2f45ecfe630dc1f0a51a1
Janne Joensuu <janne@pelitaiteilija.fi>
2023-01-13 14:31:10 +0200

    Added checkbox to toggle how moving in reverse works

--- Assets/Scripts/PlayerShip/ShipInputHandler.cs
+++ Assets/Scripts/PlayerShip/ShipInputHandler.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Ship))]
-public class ShipInputHandler : MonoBehaviour
-{
+public class ShipInputHandler : MonoBehaviour {
     public float rotationInput { get; private set; } = 0f;
     public float thrustForceInput { get; private set; } = 0f;
 
     public bool breakInput { get; private set; } = false;
 
+    private bool reverseBackwardsRotationInput = false;
+
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         rotationInput = Input.GetAxis("Horizontal");
         thrustForceInput = Input.GetAxis("Vertical");
-        if (thrustForceInput < 0f)
-            rotationInput *= -1;
         breakInput = Input.GetKey(KeyCode.Space);
+
+        if ( reverseBackwardsRotationInput && thrustForceInput < 0f) rotationInput *= -1;
     }
 }
