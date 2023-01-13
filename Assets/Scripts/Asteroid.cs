7f5231c37e7e4095979c26861ba393c37597e06f
Janne Joensuu <janne@pelitaiteilija.fi>
2023-01-13 14:29:47 +0200

    Asteroids communicate with game manager

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,45 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
-public class Asteroid : MonoBehaviour, IAmDamageable
-{
+public class Asteroid : MonoBehaviour, IAmDamageable {
     public AsteroidSizeTier size { get; private set; } = AsteroidSizeTier.Large;
 
     [SerializeField]
     private float hitpoints = 10f;
 
     private Vector2 movement = Vector2.zero;
-    private Vector2 rotation = Vector2.zero;
+    private float rotation;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        movement = new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        rotation = Random.Range(-120, 120);
+        GameManager.AsteroidCountIncrement();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(movement * Time.deltaTime);
-        transform.Rotate(rotation * Time.deltaTime);
+    void Update() {
+        transform.Translate(movement * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, rotation * Time.deltaTime);
     }
 
-    public void TakeDamage(float amount)
-    {
-        Debug.Log($"Asteroid was hit and took {amount} damage!");
+    public void TakeDamage(float amount) {
+        // Debug.Log($"Asteroid was hit and took {amount} damage!");
         hitpoints -= amount;
         if (hitpoints <= 0)
             Destroy(gameObject);
     }
 
-    public void OnDestroy()
-    {
+    public void OnDestroy() {
         Debug.Log("Asteroid destroyed");
+        GameManager.AsteroidCountDecrease();
     }
 }
 
 
-public enum AsteroidSizeTier
-{
+public enum AsteroidSizeTier {
     Large, Medium, Small
 }
