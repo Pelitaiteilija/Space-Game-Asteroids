cbe023417ceac9ce2261de77813ea94e00de4857
Janne Joensuu <janne@pelitaiteilija.fi>
2023-01-16 18:33:51 +0200

    Planning out the level / wave system

---
+++ Assets/notes.cs
@@ -0,0 +1,37 @@
+public class notes
+{
+}
+/*
+ *      LEVEL SYSTEM
+ * 
+ *  Implement some sort of level system with waves
+ *  Waves can have prefabs / enums / something you can choose to spawn from a dropdown
+ *  They are spawned with a rule that's dragged into a slot
+ *  The spawning rule contains data on where to spawn things, stuff like only on edges, but can also contain more unique stuff
+ * 
+ * 
+ *  Look into non-gameobject scripts that can hold prefabs and other scripts
+ *  Look into scriptable objects
+ *  Look into example systems from defining level generation for games
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */ 
