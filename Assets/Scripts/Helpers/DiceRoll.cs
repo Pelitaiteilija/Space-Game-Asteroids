30cddaa464522b2ab8af5677c732b29531bd2c56
Janne Joensuu <janne@pelitaiteilija.fi>
2023-01-19 14:58:13 +0200

    Defining assemblies, reorganizing script folder

--- Assets/Scripts/Helpers/DiceRoll.cs
+++ Assets/Scripts/Helpers/DiceRoll.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class DiceRoll
 {
-    int rolls;
-    int sides;
-    int modifier = 0;
+    public int rolls    { get; private set; }
+    public int sides    { get; private set; }
+    public int modifier { get; private set; }
 
-    public DiceRoll(int rolls, int sides) {
-        this.rolls = rolls;
-        this.sides = sides;
-    }
-
-    public DiceRoll(int rolls, int sides, int modifier) {
+    /// <summary>
+    /// A number of dice to roll, for example 2d6 defines a roll where two 6-sided die are thrown.
+    /// </summary>
+    /// <param name="rolls">The amount of dice to be rolled, the 1 in 1d6+N</param>
+    /// <param name="sides">The number of sides in a die, the 6 in 1d6+N</param>
+    /// <param name="modifier">Additional modifier added to the result, the N in 1d6+N. Can be a negative number.</param>
+    public DiceRoll(int rolls, int sides, int modifier = 0) {
         this.rolls = rolls;
         this.sides = sides;
         this.modifier = modifier;
     }
 
+    /// <summary>
+    /// Throw the dice defined by a DiceRoll object and return the sum of the result.
+    /// If the modifier is negative, the result may be negative as well.
+    /// </summary>
+    /// <returns></returns>
     public int Roll() {
-        int result = modifier;
-        for (int i = 0; i < rolls; i++) {
-            result += Random.Range(1, sides);
-        }
-        return result;
+        // call the static Roll() function
+        return Roll(rolls, sides, modifier);
     }
 
+    /// <summary>
+    /// Static function that returns the result of a diceroll with specific parameters
+    /// If the modifier is negative, the result may be negative as well. 
+    /// </summary>
+    /// <param name="rolls"></param>
+    /// <param name="sides"></param>
+    /// <param name="modifier"></param>
+    /// <returns></returns>
     public static int Roll(int rolls, int sides, int modifier = 0) {
         int result = modifier;
         for (int i = 0; i < rolls; i++) {
---
+++ Assets/Tests/HelperClassesEditorTests/DiceRollStaticTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class DiceRollTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void DiceRollTests_StaticRolls_SimplePasses()
+    {
+        
+    }
+
+}
