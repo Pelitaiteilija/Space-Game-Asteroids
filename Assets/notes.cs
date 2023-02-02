5bb7654da964243013d028b058533986e041da40
Pelitaiteilija <122605754+Pelitaiteilija@users.noreply.github.com>
2023-02-02 13:45:08 +0200

    Updated notes and plans
    Added plans for channels, changing zoom dynamically

--- Assets/notes.cs
+++ Assets/notes.cs
@@ -2,6 +2,21 @@ public class notes
 {
 }
 /*
+ * 
+ *      ZOOM OUT FOR FURTHER LEVELS
+ *      
+ *  Implement a system where the game can zoom in or out, and level bounds are updated with this change. 
+ *  Also look into supporting changing aspect ratios (e.g. windowed game's size is changed), and check if 
+ *  changing window size can cause objects to be pushed beyond the screen limits. 
+ * 
+ * 
+ *      CHANNELS (sound, music, game logic)
+ * 
+ *  Implement Scriptable Object -based channels using Events ( the => () stuff) which let objects send messages to 
+ *  other objects / scripts in a generic, neutral way, e.g. start sounds, stop sounds, keep playing sounds and music 
+ *  while scene changes, send asteroid created/destroyed information without referencing or knowing about the GameManager.
+ * 
+ * 
  *      LEVEL SYSTEM
  * 
  *  Implement some sort of level system with waves
@@ -10,26 +25,7 @@ public class notes
  *  The spawning rule contains data on where to spawn things, stuff like only on edges, but can also contain more unique stuff
  * 
  * 
- *  Look into non-gameobject scripts that can hold prefabs and other scripts
- *  Look into scriptable objects
  *  Look into example systems from defining level generation for games
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
  * 
  * 
  * 
