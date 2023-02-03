b3ddfae198cb8ed9305f2d0b15fa44f6a7135200
Pelitaiteilija <122605754+Pelitaiteilija@users.noreply.github.com>
2023-02-03 16:05:12 +0200

    Can now destroy objects out of bounds instead

--- Assets/Scripts/Helpers/ScreenWrapper.cs
+++ Assets/Scripts/Helpers/ScreenWrapper.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class ScreenWrapper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public bool destroyWhenOutsideOfBounds = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(ScreenBounds.AmIOutOfBounds(transform.position)) {
+        if (ScreenBounds.AmIOutOfBounds(transform.position))
+        {
+            if (destroyWhenOutsideOfBounds)
+            {
+                Destroy(gameObject);
+            }
+
             transform.position = ScreenBounds.CalculateScreenWrapPosition(transform.position);
         }
     }
