68211f31f0d844037f8a3b568f3c1beebf0eee17
Pelitaiteilija <122605754+Pelitaiteilija@users.noreply.github.com>
2023-02-07 13:02:53 +0200

    bounds and wrapping now obey better logic

--- Assets/Scripts/Helpers/ScreenBounds.cs
+++ Assets/Scripts/Helpers/ScreenBounds.cs
@@ -8,11 +8,13 @@ public class ScreenBounds : MonoBehaviour {
 
     ScreenBounds bounds;
 
-    public UnityEvent<Collider2D> ExitTriggerFired;
+    public UnityEvent<Collider2D> ExitTriggerFired { get; private set; }
 
     [SerializeField]
     [Tooltip("Increase offset to haveo objects trigger the event when they've gone past the bounds")]
-    private static float boundsOffset = 0.2f;
+    float BoundsOffset = 0.5f;
+
+    static float boundsOffset = 0.2f;
 
     private void Awake() {
         if(bounds == null) {
@@ -42,12 +44,13 @@ public class ScreenBounds : MonoBehaviour {
     }
 
     public void UpdateBoundsSize() {
+        ScreenBounds.boundsOffset = BoundsOffset;
         // set camera position to zero
         transform.position = Vector3.zero;
         // orhographicSize = half the height of the screen
         float ySize = mainCamera.orthographicSize * 2;
         // width is calculate by multiplying y with aspect ratio
-        boxCollider.size = new Vector2(ySize * mainCamera.aspect, ySize);
+        boxCollider.size = new Vector2(ySize * mainCamera.aspect + ScreenBounds.boundsOffset, ySize + ScreenBounds.boundsOffset);
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
@@ -65,21 +68,34 @@ public class ScreenBounds : MonoBehaviour {
         bool isXOutOfBounds = IsXOutOfBounds(worldPosition2D);
         bool isYOutOfBounds = IsYOutOfBounds(worldPosition2D);
 
+        // used to create and reverse coordinates of offset vector in XY, X or Y axis
         Vector2 signVector = new Vector2(Mathf.Sign(worldPosition2D.x), Mathf.Sign(worldPosition2D.y));
+        Vector2 offset = new Vector2(boundsOffset * 0.5f, boundsOffset * 0.5f);
 
+        // out in both X and Y
         if (isXOutOfBounds && isYOutOfBounds) {
-            Vector2 offset = new Vector2(boundsOffset, boundsOffset) * signVector;
+            //offset *= signVector;
             worldPosition2D = new Vector2(worldPosition2D.x * -1, worldPosition2D.y * -1) + offset;
+            Debug.DrawRay(worldPosition, offset, Color.magenta, 1.0f);
         }
+        // out in only X
         else if (isXOutOfBounds) {
-            Vector2 offset = new Vector2(boundsOffset * signVector.x, boundsOffset);
+            //Vector2 offset = new Vector2(boundsOffset * signVector.x, boundsOffset);
+            offset.y = 0;
+            offset *= signVector;
             worldPosition2D = new Vector2(worldPosition2D.x * -1, worldPosition2D.y) + offset;
+            Debug.DrawRay(worldPosition, offset, Color.cyan, 1.0f);
         }
+        // out in only Y
         else if (isYOutOfBounds) {
-            Vector2 offset = new Vector2(boundsOffset, boundsOffset * signVector.y);
+            //Vector2 offset = new Vector2(boundsOffset, boundsOffset * signVector.y);
+            offset.x = 0; 
+            offset *= signVector;
             worldPosition2D = new Vector2(worldPosition2D.x, worldPosition2D.y * -1) + offset;
+            Debug.DrawRay(worldPosition, offset, Color.yellow, 1.0f);
         }
 
+        Debug.DrawLine(worldPosition, new Vector3(worldPosition2D.x, worldPosition2D.y, worldPosition.z), Color.white, 1f);
         return new Vector3(worldPosition2D.x, worldPosition2D.y, worldPosition.z);
     }
 
